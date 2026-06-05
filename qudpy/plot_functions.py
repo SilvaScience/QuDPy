27de1cdbba7c8d34e4cf46e73d2bc096509295bc
Mathieu Desmarais <85315694+MathieuDesmarais@users.noreply.github.com>
2026-06-05 10:49:21 -0400

    Modification model
    The model is now in a loop, giving the choice to print many spectrum

--- qudpy/plot_functions.py
+++ qudpy/plot_functions.py
@@ -363,9 +363,8 @@ def silva_plot(spectra_list=None,x_val=None,y_val=None, labels=None, title_list=
             plt.xlabel(labels[0])
             plt.ylabel(labels[1])
         plt.colorbar(im, ax=axes[-1], shrink=0.7)
-
     fig.tight_layout()
-    plt.show()
+   
 
     return
 
